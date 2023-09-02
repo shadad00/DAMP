cbe6b5d7934b0d056c75ce062bad09b6399b4ce0
shadad00 <shadad@itba.edu.ar>
2023-09-02 18:29:11 -0300

    adding rounded and formatting news card

--- ser_manos/lib/design_system/cells/cards/NewsCard.dart
+++ ser_manos/lib/design_system/cells/cards/NewsCard.dart
@@ -1,7 +1,10 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:ser_manos/design_system/molecules/buttons/Short_button.dart';
 import 'package:ser_manos/design_system/tokens/font/font.dart';
 import 'package:ser_manos/design_system/tokens/colours/colours.dart';
+import 'package:ser_manos/design_system/tokens/shadows/shadows.dart';
 import 'package:ser_manos/design_system/utils/NetworkImageWrapper.dart';
 import '../../../model/News.dart';
 
@@ -15,50 +18,56 @@ class NewsCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Row(
+    return Container(
+      decoration: BoxDecoration(
+        color: SermanosColors.neutral0,
+        borderRadius: BorderRadius.circular(2),
+        boxShadow: SermanosShadows.shadow2,
+      ),
+      child: IntrinsicHeight(
+          child: Row(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
-          Column(
-            children: [
-              NetworkImageWrapper(imageUrl: news.imageUrl)
-            ],
-          ),
-          const SizedBox(width: 16),  // space between cols. 
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                'REPORTE ${news.reportId}',
-                style: const SermanosTypography.overline(
-                  color: SermanosColors.neutral75,
+          NetworkImageWrapper(imageUrl: news.imageUrl),
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'REPORTE ${news.reportId}',
+                  style: const SermanosTypography.overline(
+                    color: SermanosColors.neutral75,
+                  ),
                 ),
-              ),
-              Text(
-                news.title,
-                style: const SermanosTypography.subtitle01(
-                  color: SermanosColors.neutral100,
+                Text(
+                  news.title,
+                  style: const SermanosTypography.subtitle01(
+                    color: SermanosColors.neutral100,
+                  ),
                 ),
-              ),
-              Text(
-                news.subtitle,
-                style: const SermanosTypography.body02(
-                  color: SermanosColors.neutral75,
+                Text(
+                  news.subtitle,
+                  style: const SermanosTypography.body02(
+                    color: SermanosColors.neutral75,
+                  ),
                 ),
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: [
-                  ShortButton(
-                loading: false,
-                filled: false,
-                onPressed: () => {},
-                text: "Leer Más",
-                textColor: SermanosColors.primary100,
-              )
-                ],
-              )
-            ],
-          ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    ShortButton(
+                      loading: false,
+                      filled: false,
+                      onPressed: () => {},
+                      text: "Leer Más",
+                      textColor: SermanosColors.primary100,
+                    )
+                  ],
+                )
+              ],
+            ),
+          )
         ],
-      );
+      )),
+    );
   }
 }
