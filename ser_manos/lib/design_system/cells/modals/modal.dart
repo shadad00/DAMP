de017745769bf322b08ad59d6de6564f69a5de64
shadad00 <shadad@itba.edu.ar>
2023-09-02 12:35:46 -0300

    updating modal

--- ser_manos/lib/design_system/cells/modals/modal.dart
+++ ser_manos/lib/design_system/cells/modals/modal.dart
@@ -1,55 +1,87 @@
 import 'dart:ui';
 
 import 'package:flutter/material.dart';
-import 'package:ser_manos/design_system/molecules/buttons/Cta_button.dart';
 import 'package:ser_manos/design_system/molecules/buttons/Short_button.dart';
+import 'package:ser_manos/design_system/tokens/colours/colours.dart';
 import 'package:ser_manos/design_system/tokens/font/font.dart';
+import 'package:ser_manos/design_system/tokens/shadows/shadows.dart';
 
 class Modal extends StatelessWidget {
-  final String title;
-  final String subtitle;
-  final String confirm;
-  final String cancel;
+  final String firstText;
+  final String? secondText;
+  final String confirmButtonText;
+  final String cancelButtonText;
   final bool isLoading;
+  final Future<void> Function() onConfirm;
+
   const Modal(
-      {super.key,
-      required this.title,
-      required this.subtitle,
-      required this.confirm,
-      required this.cancel,
-      required this.isLoading});
+      {Key? key,
+      required this.firstText,
+      this.secondText,
+      required this.confirmButtonText,
+      required this.cancelButtonText,
+      required this.isLoading,
+      required this.onConfirm})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return BackdropFilter(
       filter: ImageFilter.blur(sigmaX: 5, sigmaY: 5),
-      child:
-      // TODO dialog?????
-      Column(children: [
-        Text(
-          title,
-          style: const SermanosTypography.subtitle01(),
+      child: Dialog(
+        backgroundColor: Colors.transparent,
+        surfaceTintColor: Colors.transparent,
+        child: Container(
+          padding: const EdgeInsets.fromLTRB(16, 16, 16, 8),
+          decoration: BoxDecoration(
+            color: SermanosColors.neutral0,
+            borderRadius: BorderRadius.circular(4),
+            boxShadow: SermanosShadows.shadow3,
+          ),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    firstText,
+                    style: const SermanosTypography.subtitle01(),
+                  ),
+                  if (secondText != null)
+                    Text(
+                      secondText!,
+                      style: const SermanosTypography.headline02(),
+                    ),
+                ],
+              ),
+              const SizedBox(height: 8),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  ShortButton(
+                    text: cancelButtonText,
+                    onPressed: () => Navigator.of(context).pop(false),
+                    textColor: SermanosColors.primary100,
+                    enabled: !isLoading,
+                    filled: false,
+                  ),
+                  ShortButton(
+                    text: confirmButtonText,
+                    onPressed: () async {
+                      await onConfirm();
+                    },
+                    textColor: SermanosColors.primary100,
+                    loading: isLoading,
+                    filled: false,
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
-        Text(
-          subtitle,
-          style: const SermanosTypography.headline02(),
-        ),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.end,
-          children: [
-            ShortButton(
-                onPressed: () {
-                  //TODO
-                },
-                text: confirm),
-            ShortButton(
-                onPressed: () {
-                  //TODO
-                },
-                text: cancel),
-          ],
-        )
-      ]),
+      ),
     );
   }
 }
