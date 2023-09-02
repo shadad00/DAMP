4db91b4432fccd36f79b2b0c0380c69c52e1614d
shadad00 <shadad@itba.edu.ar>
2023-09-02 17:29:38 -0300

    Creating volunteering card.

---
+++ ser_manos/lib/design_system/cells/cards/VolunteeringCard.dart
@@ -0,0 +1,93 @@
+import 'package:flutter/material.dart';
+import 'package:ser_manos/design_system/atoms/icons/icons.dart';
+import 'package:ser_manos/design_system/molecules/components/vacancies.dart';
+import 'package:ser_manos/design_system/tokens/font/font.dart';
+import 'package:ser_manos/design_system/tokens/shadows/shadows.dart';
+import 'package:ser_manos/model/Volunteering.dart';
+import '../../tokens/colours/colours.dart';
+import 'package:ser_manos/design_system/utils/NetworkImageWrapper.dart';
+
+class VolunteeringCard extends StatelessWidget {
+  final Volunteering volunteeringInformation; 
+
+  const VolunteeringCard({
+    Key? key,
+    required this.volunteeringInformation
+  }):super(key: key);
+
+  
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: () {},
+      child: Container(
+        decoration: BoxDecoration(
+          color: SermanosColors.neutral0,
+          borderRadius: BorderRadius.circular(2),
+          boxShadow: SermanosShadows.shadow2,
+        ),
+        child: ClipRRect(
+          borderRadius: BorderRadius.circular(2),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              NetworkImageWrapper(imageUrl: volunteeringInformation.imageUrl),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(16, 8, 16, 16),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: [
+                    Expanded(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            volunteeringInformation.category.toUpperCase(),
+                            style: const SermanosTypography.overline(
+                              color: SermanosColors.neutral75,
+                            ),
+                          ),
+                          Text(
+                            volunteeringInformation.name,
+                            style: const SermanosTypography.subtitle01(),
+                          ),
+                          const SizedBox(
+                            height: 4,
+                          ),
+                          Vacancies(
+                            vacancy: volunteeringInformation.capacity -
+                                volunteeringInformation.volunteerQuantity,                             
+                          ),
+                        ],
+                      ),
+                    ),
+                    Row(
+                      children: [
+                        SermanosIcons.favoriteFilled(
+                          // todo: view if the voluteering is one of the user's favourite. 
+                          status: SermanosIconStatus.activated , 
+                        ),
+                        const SizedBox(width: 23),
+                        InkWell(
+                          onTap: () => {},
+                          child: SermanosIcons.locationFilled(
+                            status: SermanosIconStatus.activated,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+
+
+}
---
+++ ser_manos/lib/model/Volunteering.dart
@@ -0,0 +1,39 @@
+import 'package:equatable/equatable.dart';
+
+class Volunteering extends Equatable {
+  final String id;
+  final String name;
+  final String category;
+  final String description;
+  final String about;
+  final String address;
+  final List<String> requirements;
+  final List<String> availability;
+  final String imageUrl;
+  final int capacity;
+  final int volunteerQuantity;
+  final DateTime creationTime;
+
+  const Volunteering({
+    required this.id,
+    required this.name,
+    required this.category,
+    required this.description,
+    required this.about,
+    required this.address,
+    required this.requirements,
+    required this.availability,
+    required this.imageUrl,
+    required this.capacity,
+    required this.volunteerQuantity,
+    required this.creationTime,
+  });
+
+  //This is used to compare with equatable. 
+  @override
+  List<Object?> get props => [id];
+
+  bool isFull() {
+    return capacity == volunteerQuantity;
+  }
+}
