3b32a304d16c1b60ef0ef6105734f53e9aff17cb
gbudoberra <gbudoberra@itba.edu.ar>
2023-09-02 12:01:28 -0300

    fix volunteering_card

--- ser_manos/lib/design_system/cells/cards/volunteering_card.dart
+++ ser_manos/lib/design_system/cells/cards/volunteering_card.dart
@@ -2,39 +2,37 @@ import 'package:flutter/material.dart';
 import 'package:ser_manos/design_system/atoms/icons/icons.dart';
 import 'package:ser_manos/design_system/molecules/components/vacancies.dart';
 
-class Volunteeering_card extends StatelessWidget{
-
+class Volunteeering_card extends StatelessWidget {
   final String title;
   final int vacancy;
   final String category;
-  const Volunteeering_card({
-    super.key, 
-    required this.title, 
-    required this.vacancy, 
-    required this.category,
-    });
-    
-      @override
-      Widget build(BuildContext context) {
-          Container(
-            child:Column(
-              children: [
-                Image(image: image),
-                Text(category),
-                Text(title),
-                Row(
-                  children: [
-                      Vacancies(vacancy: vacancy),
-                      SermanosIcons.favoriteOutlined(
-                        status: SermanosIconStatus.activated
-                        // TODO deshardcodear
-                        ),
-                      )
-                  ],
-                  ]
-                )
-            ],) 
-            )
-      }
+  final ImageProvider image;
 
+  const Volunteeering_card({
+    super.key,
+    required this.title,
+    required this.vacancy,
+    required this.category,
+    required this.image,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        child: Column(
+      children: [
+        Image(image: image),
+        Text(category),
+        Text(title),
+        Row(
+          children: [
+            Vacancies(vacancy: vacancy),
+            SermanosIcons.favoriteOutlined(status: SermanosIconStatus.activated
+                // TODO deshardcodear
+                ),
+          ],
+        )
+      ],
+    ));
+  }
 }
