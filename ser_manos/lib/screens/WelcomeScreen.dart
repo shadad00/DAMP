68531b909c34f50b8c9945583244e1dd8e5d48e5
gbudoberra <gbudoberra@itba.edu.ar>
2023-09-20 18:43:35 -0300

    add welcomeScreen

---
+++ ser_manos/lib/screens/WelcomeScreen.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:ser_manos/design_system/molecules/buttons/Cta_button.dart';
+import 'package:ser_manos/design_system/tokens/colours/colours.dart';
+import 'package:ser_manos/design_system/tokens/font/font.dart';
+import 'package:ser_manos/design_system/atoms/icons/logo.dart';
+
+class WelcomeScreen extends StatelessWidget {
+  const WelcomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // todo: add the status bar.
+    return Scaffold(
+        body: Padding(
+      padding: const EdgeInsets.all(16),
+      child: Container(
+        color: SermanosColors.neutral0,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            const Column(children: [
+              SermanosLogos.square(logoUrl: "assets/images/SquareLogo.png"),
+              SizedBox(
+                height: 30,
+              ),
+              Text("¡Bienvenido!",
+                  textAlign: TextAlign.center,
+                  style: SermanosTypography.headline01(
+                      color: SermanosColors.neutral100)),
+              const SizedBox(
+                height: 48,
+              ),
+              Text(
+                  "Nunca subestimes tu habilidad para mejorar la vida de alguien.",
+                  textAlign: TextAlign.center,
+                  style: SermanosTypography.subtitle01(
+                      color: SermanosColors.neutral100))
+            ]),
+            Column(
+              children: [
+                CtaButton(text: "Comenzar", onPressed: () {}, filled: true),
+              ],
+            )
+          ],
+        ),
+      ),
+    ));
+  }
+}
