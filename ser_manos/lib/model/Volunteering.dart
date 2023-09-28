21d47ef627b55b19d77e9c1ec7f0ccf19ff70664
shadad00 <shadad@itba.edu.ar>
2023-09-28 12:58:22 -0300

    moving volunteering creation from json to model

--- ser_manos/lib/model/Volunteering.dart
+++ ser_manos/lib/model/Volunteering.dart
@@ -29,12 +29,32 @@ class Volunteering extends Equatable {
     required this.creationTime,
   });
 
-  //This is used to compare with equatable. 
+  //This is used to compare with equatable.
   @override
   List<Object?> get props => [id];
 
-
   bool isFull() {
     return capacity == volunteerQuantity;
+  }
+
+  factory Volunteering.fromJson({
+    required String volunteeringId,
+    required Map<String, dynamic> eachVolunteering,
+  }) {
+    return Volunteering(
+        id: int.parse(volunteeringId),
+        about: eachVolunteering['about'],
+        address: eachVolunteering['address'],
+        availability: List<String>.from(
+            eachVolunteering['availability'] as List<dynamic>),
+        capacity: eachVolunteering['capacity'],
+        category: eachVolunteering['category'],
+        description: eachVolunteering['description'],
+        imageUrl: eachVolunteering['imageUrl'],
+        name: eachVolunteering['name'],
+        requirements: List<String>.from(
+            eachVolunteering['requirements'] as List<dynamic>),
+        volunteerQuantity: eachVolunteering['volunteerQuantity'],
+        creationTime: DateTime.now());
   }
 }
--- ser_manos/lib/services/implementations/FirebaseVolunteeringService.dart
+++ ser_manos/lib/services/implementations/FirebaseVolunteeringService.dart
@@ -10,24 +10,16 @@ class FirebaseVolunteeringService implements VolunteeringService {
 
   @override
   Future<Volunteering?> getVolunteeringById({required String id}) async {
-    final eachVolunteering =
+    final retrievedVolunteering =
         await firestore.collection(collectionName).doc(id).get();
-    if (!eachVolunteering.exists) {
+    if (!retrievedVolunteering.exists) {
       return null; 
     }
-    return Volunteering(
-        id: int.parse(eachVolunteering.id),
-        about: eachVolunteering['about'],
-        address: eachVolunteering['address'],
-        availability: List<String>.from(eachVolunteering['availability'] as List<dynamic>),
-        capacity: eachVolunteering['capacity'],
-        category: eachVolunteering['category'],
-        description: eachVolunteering['description'],
-        imageUrl: eachVolunteering['imageUrl'],
-        name: eachVolunteering['name'],
-        requirements: List<String>.from(eachVolunteering['requirements'] as List<dynamic>),
-        volunteerQuantity: eachVolunteering['volunteerQuantity'],
-        creationTime: DateTime.now());
+    return Volunteering.fromJson(
+      volunteeringId: retrievedVolunteering.id, 
+      eachVolunteering: Map<String, dynamic>.from(
+            retrievedVolunteering.data() as Map<String, dynamic>) 
+      ); 
   }
 
   @override
@@ -35,20 +27,10 @@ class FirebaseVolunteeringService implements VolunteeringService {
     final List<Volunteering> volunteeringList = [];
     final retrievedData = await firestore.collection(collectionName).get();
     for (var eachVolunteering in retrievedData.docs) {
-      Map<String, dynamic> eachVolunteeringData = eachVolunteering.data();
-      volunteeringList.add(Volunteering(
-          id: int.parse(eachVolunteering.id),
-          about: eachVolunteeringData['about'],
-          address: eachVolunteeringData['address'],
-          availability: List<String>.from(eachVolunteeringData['availability'] as List<dynamic>),
-          capacity: eachVolunteeringData['capacity'],
-          category: eachVolunteeringData['category'],
-          description: eachVolunteeringData['description'],
-          imageUrl: eachVolunteeringData['imageUrl'],
-          name: eachVolunteeringData['name'],
-          requirements: List<String>.from(eachVolunteeringData['requirements'] as List<dynamic>),
-          volunteerQuantity: eachVolunteeringData['volunteerQuantity'],
-          creationTime: DateTime.now()));
+      volunteeringList.add(Volunteering.fromJson(
+        volunteeringId: eachVolunteering.id, 
+        eachVolunteering: eachVolunteering.data())
+      );
     }
     return volunteeringList;
   }
