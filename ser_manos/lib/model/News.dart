774445ff826cd85efccc10cc7d1cdf580c078d2e
shadad00 <shadad@itba.edu.ar>
2023-09-28 12:52:14 -0300

    moving news Creation from json to model

--- ser_manos/lib/model/News.dart
+++ ser_manos/lib/model/News.dart
@@ -7,17 +7,31 @@ class News extends Equatable {
   final int reportId;
   final String content;
   final String imageUrl;
-  final String summary; 
+  final String summary;
 
-  const News({
-    required this.reportId,
-    required this.category,
-    required this.subTitle,
-    required this.title,
-    required this.content,
-    required this.imageUrl,
-    required this.summary
-  });
+  const News(
+      {required this.reportId,
+      required this.category,
+      required this.subTitle,
+      required this.title,
+      required this.content,
+      required this.imageUrl,
+      required this.summary});
+
+  factory News.fromJson({
+    required String newsId,
+    required Map<String, dynamic> json,
+  }) {
+    return News(
+      reportId: int.parse(newsId),
+      category: json['category'],
+      content: json['content'],
+      imageUrl: json['imageUrl'],
+      subTitle: json['subtitle'],
+      title: json['title'],
+      summary: json['summary']
+    );
+  }
 
   @override
   List<Object?> get props => [reportId];
--- ser_manos/lib/services/implementations/FirebaseNewsService.dart
+++ ser_manos/lib/services/implementations/FirebaseNewsService.dart
@@ -2,7 +2,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:ser_manos/model/News.dart';
 import 'package:ser_manos/services/interfaces%20/NewsService.dart';
 
-class FirebaseNewsService implements NewsService{
+class FirebaseNewsService implements NewsService {
   const FirebaseNewsService();
 
   static final FirebaseFirestore firestore = FirebaseFirestore.instance;
@@ -12,15 +12,7 @@ class FirebaseNewsService implements NewsService{
     final List<News> newsList = [];
     final retrievedData = await firestore.collection("news").get();
     for (var eachNew in retrievedData.docs) {
-      Map<String, dynamic> eachNewData = eachNew.data();
-      newsList.add(News(
-          reportId: int.parse(eachNew.id),
-          category: eachNewData['category'],
-          content: eachNewData['content'],
-          imageUrl: eachNewData['imageUrl'],
-          subTitle: eachNewData['subtitle'],
-          title: eachNewData['title'],
-          summary: eachNewData['summary']));
+      newsList.add(News.fromJson(newsId: eachNew.id, json: eachNew.data()));
     }
     return newsList;
   }
@@ -28,14 +20,9 @@ class FirebaseNewsService implements NewsService{
   @override
   Future<News> getNewById({required String id}) async {
     final retrievedNew = await firestore.collection("news").doc(id).get();
-    //check the id.
-    return News(
-        reportId: int.parse(retrievedNew.id),
-        category: retrievedNew['category'],
-        content: retrievedNew['content'],
-        imageUrl: retrievedNew['imageUrl'],
-        subTitle: retrievedNew['subtitle'],
-        title: retrievedNew['title'],
-        summary: retrievedNew['summary']);
+    return News.fromJson(
+        newsId: retrievedNew.id,
+        json: Map<String, dynamic>.from(
+            retrievedNew.data() as Map<String, dynamic>));
   }
 }
